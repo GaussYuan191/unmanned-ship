1f945990b661a4236d9030cfd4534c9f31bd47db
GaussYuan191 <1915059302@qq.com>
2022-03-29 18:13:21 +0800

    新增HTTPS接口

--- app.js
+++ app.js
@@ -1,104 +1,38 @@
+require('module-alias/register') // 文件别名，详细见package.json _moduleAliases
 const { dateFormat } = require("./src/utils/dateFormat");
+
 const Koa = require("koa");
-// 引入 Mock
-const Mock = require("mockjs");
+const bodyParser = require("koa-bodyparser")
+const path = require('path')
+// 引入https 以及 koa-ssl
+const https = require('https')
+const sslify = require('koa-sslify').default
+const fs = require('fs')
+const options = {
+  key: fs.readFileSync('./src/httpskey/www.yuangauss287.top.key'),
+  cert: fs.readFileSync('./src/httpskey/www.yuangauss287.top.pem'),
+ }
+ 
 
+const InitManager = require('./src/core/init')
+const catchError = require('./src/middlewares/exception')
+const static = require('koa-static')
 const app = new Koa();
-// 导入koa-router模块
-const Router = require("koa-router");
-// 创建koa-router的实例router
-const router = new Router();
-let Random = Mock.Random;
-Date.prototype.format = dateFormat;
-// 引入mysql
-const mysql = require("./src/mysql/index");
 
-// 接口
-/**
- * @api {get} http://39.101.202.100:3033/dataList 无人船数据
- * @apiDescription 获取数据
- * @apiName dataList
- * @apiGroup Data
- *
- * @apiSuccess {Number} code 后台处理状态码.
- * @apiSuccess {Object} data 请求的数据.
- * @apiSuccess {String} msg  请求信息.
- * @apiSuccessExample {json} Success-Response:
- *{
- *	
-	"code": 200,
-	"data": {
-		"id": 1643081494428,           // 数据id
-		"name": "shipInfo",            // 数据名
-		"time": "2022-01-25 11:31:34", // 数据获取的时间
-		"total": 1,                    // 数据条数
-		"battery": 80,                 // 小船电池电量
-		"speed": 3.311,                // 小船速度
-		"distance": 4                  // 与障碍物的距离
+app.use(sslify())  // 使用ssl
 
-	},
-	"msg": "操作成功"
-}
- * @apiSampleRequest http://39.101.202.100:3033/dataList
- * @apiVersion 1.0.0
- */
-router.get("/dataList", (ctx) => {
-  var data = Mock.mock({
-    code: 200,
-    "data|1": [
-      {
-        id: new Date().getTime(),
-        name: "shipInfo",
-        time: new Date().format("yyyy-MM-dd hh:mm:ss"),
-        total: 1,
-        battery: 80,
-        speed: Number(Random.float(0, 5).toFixed(3)),
-        distance: Random.integer(0, 7),
-      },
-    ],
-    msg: "操作成功",
-  });
-  ctx.body = data;
-});
-router.get("/demo", async (ctx, next) => {
-  try {
-    let data = await mysql.query("menu");
-    ctx.body = {
-      code: 200,
-      data: data,
-      msg: "操作成功",
-    };
-    await next();
-  } catch (e) {
-    ctx.body = "500";
-    await next();
-  }
-});
-
-app.use(require("koa-static")(__dirname + "/public"));
-router.get("/apidoc", (ctx) => {
-  ctx.type("text/html");
-  ctx.sendfile("public/apidoc/index.html");
-});
-app.use(async (ctx, next) => {
-  // 设置允许跨域
-  ctx.set("Access-Control-Allow-Origin", "*");
-  ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  // 请求头设置
-  ctx.set(
-    "Access-Control-Allow-Headers",
-    `Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild,x-token,sessionToken,token`
-  );
-  if (ctx.method == "OPTIONS") {
-    ctx.body = 200;
-  } else {
-    await next();
-  }
-});
-
-app.use(router.routes());
+app.use(bodyParser())
+app.use(catchError)
+app.use(static(path.join(__dirname,'./static')))
+InitManager.initCore(app)
 
 // 监听端口
-app.listen(3033, () => {
-  console.log("服务器已启动，http://localhost:3033");
+const port = process.env.PORT || 3033;
+https.createServer(options, app.callback()).listen(port, (err) => {
+  if (err) {
+    console.log('服务启动出错', err);
+  } else {
+    console.log('guessWord-server运行在' + port + '端口');
+    console.log(app)
+  }	
 });
