2e199fc059ad3cc611edbab950fad1b084692c91
GaussYuan191 <1915059302@qq.com>
2022-03-31 15:54:57 +0800

    新增 无人船与用户绑定接口

---
+++ src/app/api/v1/relation.js
@@ -0,0 +1,18 @@
+const Router = require("koa-router");
+
+
+const { Auth } = require("../../../middlewares/auth");
+const { UserShipRelation } = require("../../models/userShipRelation");
+const router = new Router({
+  prefix: "/v1/relation",
+});
+
+router.post("/add", new Auth().m, async (ctx, next) => {
+  const uid = ctx.auth.uid;
+  const sid = ctx.request.body.sid;
+  await UserShipRelation.add(uid, sid);
+  throw new global.errs.Success();
+});
+
+
+module.exports = router;
