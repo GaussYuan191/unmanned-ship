55dbaf49d472f1d6b89a798f6befb53717aef6af
GaussYuan191 <1915059302@qq.com>
2022-03-31 11:35:28 +0800

    修改 样式

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,42 +1,42 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 const { Sequelize, Model } = require("sequelize");
 
 const { sequelize } = require("../../core/db");
 
 class User extends Model {
-  static async verifyEmailPassword(email, plainPassword){
+  static async verifyEmailPassword(email, plainPassword) {
     const user = await User.findOne({
       where: {
-        email
-      }
-    })
-    if(!user) {
-      throw new global.errs.AuthFailed('用户不存在')
+        email,
+      },
+    });
+    if (!user) {
+      throw new global.errs.AuthFailed("用户不存在");
     }
-    const correct = bcrypt.compareSync(plainPassword, user.password)
-    if(!correct) {
-      throw new global.errs.AuthFailed('密码不正确')
+    const correct = bcrypt.compareSync(plainPassword, user.password);
+    if (!correct) {
+      throw new global.errs.AuthFailed("密码不正确");
     }
-    return user
+    return user;
   }
-  static async getUserByOpenid(openid){
+  static async getUserByOpenid(openid) {
     const user = await User.findOne({
-      where:{
-        openid
-      }
-    })
-    return user
+      where: {
+        openid,
+      },
+    });
+    return user;
   }
-  static async registerByOpenid(openid){
+  static async registerByOpenid(openid) {
     return await User.create({
-      openid
-    })
+      openid,
+    });
   }
 }
 
 User.init(
   {
-    id: {
+    uid: {
       type: Sequelize.INTEGER,
       //设置为主键  关系型数据库
       //不能重复 不能为空
@@ -44,33 +44,31 @@ User.init(
       //如果只是简单的自增，并发条件下可能出错
       primaryKey: true,
       //设置自增
-      autoIncrement: true
+      autoIncrement: true,
     },
     nickname: Sequelize.STRING,
     email: {
       type: Sequelize.STRING(128),
-      unique: true
+      unique: true,
     },
     password: {
-      type:Sequelize.STRING,
+      type: Sequelize.STRING,
       //set方法，自动加密
-      set(val){
-        const salt = bcrypt.genSaltSync(10)
-        const psw = bcrypt.hashSync(val,salt)
-        this.setDataValue('password',psw)
-      }
+      set(val) {
+        const salt = bcrypt.genSaltSync(10);
+        const psw = bcrypt.hashSync(val, salt);
+        this.setDataValue("password", psw);
+      },
     },
     openid: {
       type: Sequelize.STRING(64),
-      unique: true
-    }
+      unique: true,
+    },
   },
-  { sequelize,
-    tableName: 'user'
-  }
+  { sequelize, tableName: "user" }
 );
 
 module.exports = {
-  User
-}
+  User,
+};
 //数据迁移 SQL 更新 风险
