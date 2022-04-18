c383b7efb34f1033ac232eca4f20ccf64db2c5d7
GaussYuan191 <1915059302@qq.com>
2022-04-18 22:28:46 +0800

    新增 计划实体、接口

--- src/app/api/v1/ship.js
+++ src/app/api/v1/ship.js
@@ -1,6 +1,7 @@
 const Router = require("koa-router");
 
 const { shipData } = require("../../models/shipData");
+const { Ship } = require("../../models/ship");
 const { Auth } = require("../../../middlewares/auth");
 
 const router = new Router({
@@ -11,6 +12,18 @@ router.get("/getData", new Auth().m, async (ctx, next) => {
   const uid = ctx.auth.uid;
   let data = await shipData.getData(uid)
   throw new global.errs.Success(data);
+});
+router.post("/getShipList", async (ctx, next) => {
+  let queryParam = ctx.request.body;    
+  
+  let data = await Ship.getShipData(queryParam)
+  throw new global.errs.Success(data);
+});
+router.post("/uploadData", new Auth().m, async (ctx, next) => {
+  // const uid = ctx.auth.uid;
+  // let data = await shipData.getData(uid)
+  // throw new global.errs.Success(data);
+  console.log("上传的数据--", ctx);
 });
 
 
