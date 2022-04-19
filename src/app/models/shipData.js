b7dfdb68ea98ebeffffefffeaee9b68a44b764ae
GaussYuan191 <1915059302@qq.com>
2022-04-19 22:14:55 +0800

    新增 无人船行驶数据支持websocket

--- src/app/models/shipData.js
+++ src/app/models/shipData.js
@@ -1,36 +1,47 @@
 const { Sequelize, Model } = require("sequelize");
 const { sequelize } = require("../../core/db");
 const { UserShipRelation } = require("./userShipRelation");
-const {Ship} = require("./ship");
+const { Ship } = require("./ship");
 
 class shipData extends Model {
-  static async getData(uid) {
-    let userShipRelation = await UserShipRelation.findOne({
-      where: {
-        uid: uid
-      }
-    })
-    if (!userShipRelation) {
-      throw new global.errs.QueryError("该用户没有所属的无人船, 请先绑定无人船", 60001);
-    }
+  static async getData(queryParam) {
+    let { reqPageNum = "", reqPageSize = "", sid = "" } = queryParam;
+    reqPageNum = Number(reqPageNum);
+    reqPageSize = Number(reqPageSize);
+    console.log("请求参数", queryParam, sid);
     let ship = await Ship.findOne({
       where: {
-        sid: userShipRelation.sid
-      }
-    })
+        sid: sid,
+      },
+    });
     if (!ship) {
-      throw new global.errs.QueryError("该无人船的数据不存在",  60002);
+      throw new global.errs.QueryError("该无人船的数据不存在", 60002);
     }
     let data = await shipData.findOne({
+      distinct: true,
+      order: [["created_at", "DESC"]],
       where: {
-        sid: ship.sid
+        sid: ship.sid,
       },
     });
 
+    
+    data.dataValues.location = {
+      longitude: 113.032091,
+      latitude: 28.236334,
+    };
+    // console.log(item);
+
     if (!data) {
-      throw new global.errs.QueryError("暂无无人船的航行数据",  60003);
+      throw new global.errs.QueryError("暂无无人船的航行数据", 60003);
     }
-    return {...data.dataValues, ...ship.dataValues};
+    return { ...data.dataValues, ...ship.dataValues };
+  }
+  static async addData(param) {
+    await Ship.updateStatus({ sid: param.sid, status: param.status });
+    let data = shipData.create(param);
+    // console.log(data);
+    return data;
   }
 }
 shipData.init(
@@ -43,7 +54,7 @@ shipData.init(
       //如果只是简单的自增，并发条件下可能出错
       primaryKey: true,
       //设置自增
-      autoIncrement: true
+      autoIncrement: true,
     },
     sid: Sequelize.INTEGER,
     battery: Sequelize.INTEGER,
@@ -54,14 +65,15 @@ shipData.init(
     hum: Sequelize.INTEGER,
     speed: Sequelize.DECIMAL(10, 2),
     algae_finish: Sequelize.FLOAT,
-    algae_weight: Sequelize.FLOAT
+    algae_weight: Sequelize.FLOAT,
   },
-  { sequelize,
-    tableName: 'ship_data',
+  {
+    sequelize,
+    tableName: "ship_data",
     // timestamps: false,
   }
 );
 
 module.exports = {
-  shipData
-}
+  shipData,
+};
