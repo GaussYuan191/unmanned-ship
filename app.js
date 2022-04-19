e1366bc7d664cbf4cec2f25b027181a94c344573
GaussYuan191 <1915059302@qq.com>
2022-04-19 22:11:47 +0800

    新增 websocket支持

--- app.js
+++ app.js
@@ -12,12 +12,15 @@ const options = {
   key: fs.readFileSync('./src/httpskey/www.yuangauss287.top.key'),
   cert: fs.readFileSync('./src/httpskey/www.yuangauss287.top.pem'),
  }
- 
+ // koa-websocket是koa封装好的websocket功能
+const websocket = require('koa-websocket')
+const ws = require('ws') // websocket
+const app = websocket(new Koa())
 
 const InitManager = require('./src/core/init')
 const catchError = require('./src/middlewares/exception')
 const static = require('koa-static')
-const app = new Koa();
+// const app = new Koa();
 // CORS是一个W3C标准，全称是"跨域资源共享"（Cross-origin resource sharing）。
 const cors = require('koa2-cors');
 app.use(cors({
@@ -56,6 +59,15 @@ const port = process.env.PORT || 8080;
 //     console.log(app)
 //   }	
 // });
-app.listen(port, () => {
+
+let server = app.listen(port, () => {
   console.log(`服务器已启动，http://127.0.0.1:${port}`);
 });
+
+// websocket
+// const wss = new ws.Server({ server })
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message)
+//   })
+// })
